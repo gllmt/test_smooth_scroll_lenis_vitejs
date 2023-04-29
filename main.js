18b5842821b0a33f1a38493c1e5ed20b470950dd
Pierre Guillemot <pierre.guillemot@adveris.fr>
2023-04-29 11:52:30 +0200

    fix lenis and add utils js

--- main.js
+++ main.js
@@ -19,23 +19,56 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 * --------------------
 */
 // console.log("Js works here !")
+// // Remove Fooc effect :
+// // Helper function
+let domReady = (cb) => {
+  document.readyState === 'interactive' || document.readyState === 'complete'
+    ? cb()
+    : document.addEventListener('DOMContentLoaded', cb);
+};
+
+domReady(() => {
+// Display body when DOM is loaded
+document.body.style.visibility = 'visible';
+});
+
 // smooth scroll Lenis locomotive scroll alternative !
-const lenis = new Lenis({
-  duration: 1.2,
-  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-  direction: 'vertical', // vertical, horizontal
-  gestureDirection: 'vertical', // vertical, horizontal, both
-  smooth: true,
-  mouseMultiplier: 1,
-  smoothTouch: false,
-  touchMultiplier: 2,
-  infinite: false,
-})
+// const lenis = new Lenis({
+//   duration: 1.2,
+//   easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+//   direction: 'vertical', // vertical, horizontal
+//   gestureDirection: 'vertical', // vertical, horizontal, both
+//   smooth: true,
+//   mouseMultiplier: 1,
+//   smoothTouch: false,
+//   touchMultiplier: 2,
+//   infinite: false,
+// })
 
 //get scroll value check if work in console
 // lenis.on('scroll', ({ scroll, limit, velocity, direction, progress }) => {
 //   console.log({ scroll, limit, velocity, direction, progress })
 // })
+
+// function raf(time) {
+//   lenis.raf(time)
+//   requestAnimationFrame(raf)
+// }
+
+// requestAnimationFrame(raf)
+
+const lenis = new Lenis({
+  duration: 1.5, 
+  easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)), // https://easings.net
+	orientation: 'vertical', 
+  smoothWheel: true,
+  smoothTouch: false,
+  touchMultiplier: 1.5
+})
+
+lenis.on('scroll', (e) => {
+  // console.log(e)
+})
 
 function raf(time) {
   lenis.raf(time)
@@ -44,24 +77,31 @@ function raf(time) {
 
 requestAnimationFrame(raf)
 
-// gsap intro anim 
-const TLINTRO = gsap.timeline({
-    default: {
-      duration: 0,
-      ease: 'sine'
-    }
-})
-
-TLINTRO
-    .to('.title', { y: 0, duration: 0.3, delay: .3})
-    .to('.link', { x: 0, duration: 0.3, delay: .5},'-=0.4')
-    .to('.text', { autoAlpha: 1, duration: 0.5, delay: .5}, '-=0.3')
-// ---
 
 // gsap ScrollTrigger 
 // console.log(ScrollTrigger);
 gsap.registerPlugin(ScrollTrigger)
 
+// Add lenis to scrollTrigger
+lenis.on('scroll', ScrollTrigger.update)
+
+gsap.ticker.add((time)=>{
+  lenis.raf(time * 1000)
+})
+
+// gsap intro anim 
+const TLINTRO = gsap.timeline({
+  default: {
+    duration: 0,
+    ease: 'sine'
+  }
+})
+
+TLINTRO
+  .to('.title', { y: 0, duration: 0.3, delay: .3})
+  .to('.link', { x: 0, duration: 0.3, delay: .5},'-=0.4')
+  .to('.text', { autoAlpha: 1, duration: 0.5, delay: .5}, '-=0.3')
+// ---
 
 // parallax image test
 gsap.to('.img', {
@@ -91,8 +131,26 @@ gsap.to('h3.title-scroll', {
 })
 
 
-// -----
-// for smooth anchors with Lenis
+// ----- Lenis utils -----
+
+// allow scrolling on overflow elements :
+// document
+// 	.querySelector('.over--scroll')
+//   .setAttribute("onwheel", "event.stopPropagation()");
+
+// stop page scrolling :
+// document.querySelector('.stop--scroll')
+// 	.onclick = function() { 
+//   	lenis.stop();
+// }
+
+// start page scrolling :
+// document.querySelector('.start--scroll')
+// 	.onclick = function() { 
+//   	lenis.start();
+// }
+
+// for smooth anchors with Lenis :
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
